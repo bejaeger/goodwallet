2813c6fcc642753d486c951160c555844ffabefc
Benjamin Paul Jaeger <benjamin.jaeger@cern.ch>
2021-03-19 17:06:57 +0100

    Add dialog service

--- lib/ui/views/goodcauses/single_project_view_mobile.dart
+++ lib/ui/views/goodcauses/single_project_view_mobile.dart
@@ -3,8 +3,6 @@ import 'package:good_wallet/datamodels/goodcauses/global_giving_project_model.da
 import 'package:good_wallet/ui/views/goodcauses/single_project_viewmodel.dart';
 import 'package:good_wallet/utils/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
-import 'package:functional_widget_annotation/functional_widget_annotation.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
 
 class SingleProjectViewMobile extends StatelessWidget {
   final GlobalGivingProjectModel project;
@@ -48,22 +46,7 @@ class SingleProjectViewMobile extends StatelessWidget {
                               decoration: InputDecoration(hintText: '\$'))),
                       Expanded(
                         child: ElevatedButton(
-                          onPressed: () {
-                              // Show a basic widget
-                            Alert(
-                              context: context, title: "Confirmation", 
-                              desc: "Are you sure that you want to donate N good dollars to ${project.title}",
-                              buttons: [
-                                DialogButton(
-                                  child: Text(
-                                    "Confirm"),
-                                    ),
-                                DialogButton(child: 
-                                Text("Cancel"),
-                                onPressed: () => Navigator.pop(context),),
-                              ]
-                              ).show();
-                          },
+                          onPressed: ()  =>  model.showConfirmationDialog(project.title),
                           child: Text('Donate'),
                         ),
                       ),
--- lib/ui/views/goodcauses/single_project_viewmodel.dart
+++ lib/ui/views/goodcauses/single_project_viewmodel.dart
@@ -4,9 +4,25 @@ import 'package:good_wallet/app/locator.dart';
 import 'package:good_wallet/datamodels/goodcauses/global_giving_project_model.dart';
 import 'package:good_wallet/datamodels/goodcauses/good_wallet_project_model.dart';
 import 'package:good_wallet/ui/views/base_viewmodel.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class SingleProjectViewModel extends BaseModel {
+  final _dialogService = locator<DialogService>();
+
   void donationPopup() {
     print('funktioniert');
+  }
+
+  Future showConfirmationDialog(String projectTitle) async {
+    DialogResponse response = await _dialogService.showConfirmationDialog(
+      title: 'Confirmation',
+      description:
+          "Are you sure that you want to donate N good dollars to $projectTitle",
+      confirmationTitle: 'Yes',
+      dialogPlatform: DialogPlatform.Material,
+      cancelTitle: 'No',
+    );
+
+    print('DialogResponse: ${response?.confirmed}');
   }
 }
