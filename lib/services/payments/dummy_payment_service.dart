dade2bc6e59e5d846e31fc450911fb5722341226
Patrick Mayerhofer <patrickmayerhofer@MacBook-Pro-2.local>
2021-03-26 19:18:43 -0700

    Implemented the dummy version of processTransaction. To do: when donating, the donation amount on the server should also change. Currently, just the balance decreases.

--- lib/services/payments/dummy_payment_service.dart
+++ lib/services/payments/dummy_payment_service.dart
@@ -11,7 +11,7 @@ class DummyPaymentService {
   final CollectionReference _paymentsCollectionReference =
       FirebaseFirestore.instance.collection("payments");
 
-  Future processDonation(String uid, String projectId) async {
+  Future processDonation(String uid, String projectTitle, int amount) async {
     try {
       // get reference to collection we want to add a document to
       // that holds the relevant information for the donation.
@@ -19,17 +19,17 @@ class DummyPaymentService {
 
       // Create DonationModel. This is a dummy for now. Here we need
       // the actual information to be added
-      DonationModel donationModelDummyofDummy = DonationModel.fromMap({
+      DonationModel donationModelDummy = DonationModel.fromMap({
         "projectId": "DummyId",
-        "projectName": "DummyProject",
-        "amount": 700,
+        "projectName": projectTitle,
+        "amount": amount,
         "currency": "cad",
         "status": "dummy",
       });
 
       // Add new document to cloud firestore. This will trigger the server function
       // to update the good wallet!
-      docRef.add(donationModelDummyofDummy.toJson());
+      docRef.add(donationModelDummy.toJson());
     } catch (e) {
       log.e("Couldn't process donation: ${e.toString()}");
       rethrow;
@@ -40,7 +40,6 @@ class DummyPaymentService {
     // to be implemented
     // similar to processDonation above but use add TransactionModel to firestore
     // collection 'payments' not 'users/<userId>/donations/{donationId}' as above
-
     try {
       TransactionModel _transactionModelDummyofDummy =
           TransactionModel.fromMap({
--- lib/ui/views/goodcauses/single_project_viewmodel.dart
+++ lib/ui/views/goodcauses/single_project_viewmodel.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:good_wallet/app/app.locator.dart';
 import 'package:good_wallet/services/payments/dummy_payment_service.dart';
+import 'package:good_wallet/services/userdata/user_data_service.dart';
 import 'package:good_wallet/ui/views/common_viewmodels/base_viewmodel.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class SingleProjectViewModel extends BaseModel {
   final _dialogService = locator<DialogService>();
   final _dummyPaymentService = locator<DummyPaymentService>();
+  final _userDataService = locator<UserDataService>();
 
   Future showConfirmationDialog(
       String projectTitle, String donationAmount) async {
-    DialogResponse response = await _dialogService.showConfirmationDialog(
+      try {
+        DialogResponse response = await _dialogService.showConfirmationDialog(
       title: 'Confirmation',
       description:
           "Are you sure that you want to donate $donationAmount good dollars to $projectTitle",
@@ -18,8 +21,15 @@ class SingleProjectViewModel extends BaseModel {
       dialogPlatform: DialogPlatform.Material,
       cancelTitle: 'No',
     );
-
-    print('DialogResponse: ${response?.confirmed}');
+    if (response.confirmed) {
+      _dummyPaymentService.processDonation(_userDataService.currentUser.id, projectTitle, int.parse(donationAmount));
+    }
+    print('DialogResponse: ${response.confirmed}');
+      }  catch (e) {
+        log.e("Couldn't process donation: ${e.toString()}");
+        rethrow;
+    }
+      
   }
 
   Future showAmountTooHighDialog(String donationAmount) async{
