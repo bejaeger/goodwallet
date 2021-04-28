6a3782335891550f29ac322027458f14c49ab5d6
Benjamin Paul Jaeger <benjamin.jaeger@cern.ch>
2021-04-28 20:50:39 +0200

    Fix getting initials

--- lib/utils/datamodel_helpers.dart
+++ lib/utils/datamodel_helpers.dart
@@ -8,7 +8,10 @@ String getInitialsFromName(String name) {
   List<String> splitName = name.split(" ");
   List<String> initials = [];
   for (var i = 0; i < 2; i++) {
-    if (splitName.length >= i + 1) initials.add(splitName[i][0].toUpperCase());
+    if (splitName[i].length > 0) {
+      if (splitName.length >= i + 1)
+        initials.add(splitName[i][0].toUpperCase());
+    }
   }
   return initials.join("");
 }
