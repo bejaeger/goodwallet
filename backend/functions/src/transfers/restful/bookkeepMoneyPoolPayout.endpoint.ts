65f5f43e4843b373f07034c8dde39fe43125d48d
Benjamin Paul Jaeger <benjamin.jaeger@cern.ch>
2021-08-07 00:52:10 -0700

    need to add CORS to avoid Access-Control-Allow-Origin error

--- backend/functions/src/transfers/restful/bookkeepMoneyPoolPayout.endpoint.ts
+++ backend/functions/src/transfers/restful/bookkeepMoneyPoolPayout.endpoint.ts
@@ -9,6 +9,7 @@ import { FirestoreManager } from '../../system/firestoreManager';
 
 const admin = require("firebase-admin");
 const db = admin.firestore();
+const cors = require('cors');
 
 // enable short hand for console.log()
 function log(message: string) { console.log(`bookkeepMoneyPoolPayout | ${message}`); }
@@ -17,57 +18,59 @@ function log(message: string) { console.log(`bookkeepMoneyPoolPayout | ${message
 // takes money pool payout data and updates user good wallets.
 // This will add the money pool payout document and also
 // create single money transfer documents.
-export default new Post(async (request: Request, response: Response) => {
+export default new Post((request: Request, response: Response) => {
+    cors()(request, response, async () => {
 
-    // TODO: Test if requested payout amount is valid!
-    const dbManager = new FirestoreManager(db);
-    const gwBookkeeper = new GoodWalletBookkeeper(db);
-
-    try {
-
-        let data = getPayload(request.body);
-        log("Entering function with payload:")
-        console.log(data);
-
-        // add money pool payout to batch
-        const docRef = dbManager.createMoneyPoolPayoutDocument();
-        data["createdAt"] = admin.firestore.FieldValue.serverTimestamp();
-        data["payoutId"] = docRef.id;
-
-        const transfersDetails = data["transfersDetails"];
-        const moneyPool = data["moneyPool"];
-        const deleteMoneyPool = data["deleteMoneyPool"];
+        // TODO: Test if requested payout amount is valid!
+        const dbManager = new FirestoreManager(db);
+        const gwBookkeeper = new GoodWalletBookkeeper(db);
 
         try {
-            await db.runTransaction(async (t: any) => {
 
-                // A. update stats for each transfersDetails document
-                await gwBookkeeper.updateStatsOnMoneyPoolPayout(t, transfersDetails, moneyPool, deleteMoneyPool);
+            let data = getPayload(request.body);
+            log("Entering function with payload:")
+            console.log(data);
 
-                // B. add single money transfer documents
-                gwBookkeeper.addSingleMoneyTransferDocuments(t, data, docRef.id);
+            // add money pool payout to batch
+            const docRef = dbManager.createMoneyPoolPayoutDocument();
+            data["createdAt"] = admin.firestore.FieldValue.serverTimestamp();
+            data["payoutId"] = docRef.id;
 
-                // C. add full money pool payout document as well
-                t.set(docRef, data);
-            });
+            const transfersDetails = data["transfersDetails"];
+            const moneyPool = data["moneyPool"];
+            const deleteMoneyPool = data["deleteMoneyPool"];
 
-            log(`Transaction success!`);
-        } catch (e) {
-            log(`Transaction failure: ${e}`); // log error
-            throw e; // and rethrow
+            try {
+                await db.runTransaction(async (t: any) => {
+
+                    // A. update stats for each transfersDetails document
+                    await gwBookkeeper.updateStatsOnMoneyPoolPayout(t, transfersDetails, moneyPool, deleteMoneyPool);
+
+                    // B. add single money transfer documents
+                    gwBookkeeper.addSingleMoneyTransferDocuments(t, data, docRef.id);
+
+                    // C. add full money pool payout document as well
+                    t.set(docRef, data);
+                });
+
+                log(`Transaction success!`);
+            } catch (e) {
+                log(`Transaction failure: ${e}`); // log error
+                throw e; // and rethrow
+            }
+
+            log("Responding with:")
+            console.log(ResponseHandler.returnSuccess());
+            response.status(StatusCodes.OK).send(
+                ResponseHandler.returnSuccess()
+            );
+
+        } catch (error) {
+            log(error);
+            response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ResponseHandler.returnError(error.message));
         }
-
-        log("Responding with:")
-        console.log(ResponseHandler.returnSuccess());
-        response.status(StatusCodes.OK).send(
-            ResponseHandler.returnSuccess()
-        );
-
-    } catch (error) {
-        log(error);
-        response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ResponseHandler.returnError(error.message));
-    }
-
+    });
 }
 );
+
 
--- backend/functions/src/transfers/restful/bookkeepMoneyTransfer.endpoint.ts
+++ backend/functions/src/transfers/restful/bookkeepMoneyTransfer.endpoint.ts
@@ -6,107 +6,111 @@ import { getPayload } from '../../system/helperFunctions';
 import { StatusCodes } from 'http-status-codes';
 import { ResponseHandler } from '../../system/responseHandler';
 import { FirestoreManager } from '../../system/firestoreManager';
-// const functions = require('firebase-functions');
 
 const admin = require("firebase-admin");
 const db = admin.firestore();
+const cors = require('cors');
 
 // enable short hand for console.log()
 function log(message: string) { console.log(`bookkeepMoneyTransfer | ${message}`); }
 
-export default new Post(async (request: Request, response: Response) => {
+export default new Post((request: Request, response: Response) => {
+    cors()(request, response, async () => {
 
-    try {
-        log("Calling function with request body:");
-        let data: any = getPayload(request.body);
-        console.log(data);
 
-        // TODO: Check whether input request is valid!
+        try {
+            log("Calling function with request body:");
+            let data: any = getPayload(request.body);
+            console.log(data);
 
-        const gwBookkeeper = new GoodWalletBookkeeper(db);
-        const dbHandler = new FirestoreManager(db);
+            // TODO: Check whether input request is valid!
 
-        const transferDetails = data["transferDetails"];
-        const type = data["type"];
+            const gwBookkeeper = new GoodWalletBookkeeper(db);
+            const dbHandler = new FirestoreManager(db);
 
-        let docRef = dbHandler.createMoneyTransferDocument();
-        data["createdAt"] = admin.firestore.FieldValue.serverTimestamp();
-        data["transferId"] = docRef.id;
+            const transferDetails = data["transferDetails"];
+            const type = data["type"];
 
-        // prepare return Data
-        let returnData: any = { transferId: docRef.id };
+            let docRef = dbHandler.createMoneyTransferDocument();
+            data["createdAt"] = admin.firestore.FieldValue.serverTimestamp();
+            data["transferId"] = docRef.id;
 
-        ///////////////////////////////////////////////////////////////
-        // Donation
-        if (type === "User2Project") {
-            // need to run a transaction here because we need to 
-            // fetch a file before writing back to the db
-            const senderId = transferDetails["senderId"];
-            const amount = transferDetails["amount"];
-            const sourceType = transferDetails["sourceType"];
-            const projectInfo = data["projectInfo"];
+            // prepare return Data
+            let returnData: any = { transferId: docRef.id };
 
-            try {
-                await db.runTransaction(async (t: any) => {
-                    const totalDonationChange = await gwBookkeeper.updateStatsOnDonation(t, senderId, amount, sourceType, projectInfo);
-                    returnData = { ...returnData, ...totalDonationChange };
-                    t.set(docRef, data);
-                });
-            } catch (e) {
-                log(`Transaction failure: ${e}`); // log error
-                throw e; // and rethrow
+            ///////////////////////////////////////////////////////////////
+            // Donation
+            if (type === "User2Project") {
+                // need to run a transaction here because we need to 
+                // fetch a file before writing back to the db
+                const senderId = transferDetails["senderId"];
+                const amount = transferDetails["amount"];
+                const sourceType = transferDetails["sourceType"];
+                const projectInfo = data["projectInfo"];
+
+                try {
+                    await db.runTransaction(async (t: any) => {
+                        const totalDonationChange = await gwBookkeeper.updateStatsOnDonation(t, senderId, amount, sourceType, projectInfo);
+                        returnData = { ...returnData, ...totalDonationChange };
+                        t.set(docRef, data);
+                    });
+                } catch (e) {
+                    log(`Transaction failure: ${e}`); // log error
+                    throw e; // and rethrow
+                }
             }
-        }
 
-        ////////////////////////////////////////////////////////////
-        // Money Pool Contribution
-        if (type === "User2MoneyPool") {
-            const senderId = transferDetails["senderId"];
-            const amount = transferDetails["amount"];
-            const moneyPoolInfo = data["moneyPoolInfo"];
-            const moneyPoolId = moneyPoolInfo["moneyPoolId"];
+            ////////////////////////////////////////////////////////////
+            // Money Pool Contribution
+            if (type === "User2MoneyPool") {
+                const senderId = transferDetails["senderId"];
+                const amount = transferDetails["amount"];
+                const moneyPoolInfo = data["moneyPoolInfo"];
+                const moneyPoolId = moneyPoolInfo["moneyPoolId"];
 
-            try {
-                await db.runTransaction(async (t: any) => {
-                    await gwBookkeeper.updateStatsOnMoneyPoolContribution(t, moneyPoolId, moneyPoolInfo, amount, senderId);
-                    t.set(docRef, data);
-                });
-            } catch (e) {
-                log(`Transaction failure: ${e}`); // log error
-                throw e;
+                try {
+                    await db.runTransaction(async (t: any) => {
+                        await gwBookkeeper.updateStatsOnMoneyPoolContribution(t, moneyPoolId, moneyPoolInfo, amount, senderId);
+                        t.set(docRef, data);
+                    });
+                } catch (e) {
+                    log(`Transaction failure: ${e}`); // log error
+                    throw e;
+                }
             }
+
+            /////////////////////////////////////////////////////////////////
+            // Peer 2 Peer Transfer
+            if (type === "User2User") {
+                // can run batch here (cause we only need write operations) 
+                // batch hasa the advantage that it works offline
+                const batch = db.batch();
+                batch.set(docRef, data);
+                const recipientId = transferDetails["recipientId"];
+                const senderId = transferDetails["senderId"];
+                const amount = transferDetails["amount"];
+                gwBookkeeper.updateStatsOnUser2UserTransfer(batch, recipientId, senderId, amount)
+                    .catch(() => log("Error: Could not add updates of user to user transfer documents to batch commit)"));
+
+                // await all async calls
+                await batch.commit();
+            }
+
+
+            response.status(StatusCodes.OK).send(
+                ResponseHandler.returnData(returnData)
+            );
+
+        } catch (error) {
+            log(error);
+            response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ResponseHandler.returnError(error.message));
         }
 
-        /////////////////////////////////////////////////////////////////
-        // Peer 2 Peer Transfer
-        if (type === "User2User") {
-            // can run batch here (cause we only need write operations) 
-            // batch hasa the advantage that it works offline
-            const batch = db.batch();
-            batch.set(docRef, data);
-            const recipientId = transferDetails["recipientId"];
-            const senderId = transferDetails["senderId"];
-            const amount = transferDetails["amount"];
-            gwBookkeeper.updateStatsOnUser2UserTransfer(batch, recipientId, senderId, amount)
-            .catch(() => log("Error: Could not add updates of user to user transfer documents to batch commit)"));
-
-            // await all async calls
-            await batch.commit();
-        }
-
-
-        response.status(StatusCodes.OK).send(
-            ResponseHandler.returnData(returnData)
-        );
-
-    } catch (error) {
-        log(error);
-        response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ResponseHandler.returnError(error.message));
-    }
-
+    });
 
 
 }
 );
+
 
 
